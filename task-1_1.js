efd0fa0020b1f1700ae07fab963d0ec713f1cbca
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-07-16 19:00:12 +0300

    task 1_1, 1_2

--- task-1_1.js
+++ task-1_1.js
@@ -1,4 +1,5 @@
-const nameProduct = "Генератор";
-let priceProduct =  2000;
-const rezult = `Обрано "${nameProduct}", ціна за одиницю ${priceProduct} кредитів`;
-console.log(rezult);
+let name = "Генератор"
+let price =  1000
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів`)
+price =  2000
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів`)
